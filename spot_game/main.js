4fd9e5741a565117e377fdabe44039653a1463cb
KseniyaAnikin <100955276+KseniyaAnikin@users.noreply.github.com>
2023-01-24 13:57:02 +0700

    fix timer

--- spot_game/main.js
+++ spot_game/main.js
@@ -2,8 +2,10 @@ const startBtn = document.querySelector('#start');
 const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
 let time = 0;
+let score = 0;
 const timeEl = document.querySelector('#time');
 const board = document.querySelector('#board');
+const COLORS = ['red', 'blue', 'green', 'yellow', 'white', 'lime', 'pink'];
 
 startBtn.addEventListener('click', (event)=> {
  event.preventDefault();
@@ -19,9 +21,10 @@ timeList.addEventListener('click', event => {
 });
 
 function startGame(){
-  setInterval(decreaseTime, 1000)
+  setInterval(decreaseTime, 1000);
+  createRandomSpot();
   setTime(time);
-}
+};
 
 function decreaseTime(){
   if(time === 0){
@@ -33,24 +36,45 @@ function decreaseTime(){
   }
   setTime(current);
   }  
-}
+};
 
 function setTime(value){
   timeEl.innerHTML = `00:${value}`;
-}
+};
 
 function finishGame(){
-
-}
+  board.innerHTML = `<h1>Score:${score}<h1/>`;
+  timeEl.parentNode.classList.add('hide');
+};
 
 function createRandomSpot(){
   const spot = document.createElement('div');
+  const size = getRandomNumber(10, 40);
+  const {width, height} = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
   spot.classList.add('circle');
-  spot.style.width = '15px';
-  spot.style.height = '15px';
-  board.append('spot');
-}
+  spot.style.width = `${size}px`;
+  spot.style.height = `${size}px`;
+  spot.style.top = `${y}px`;
+  spot.style.left = `${x}px`;
+  spot.style.background = getRandomColor();
+  board.append(spot);
+};
 
-startGame();
+function getRandomNumber(min, max){
+  return Math.round(Math.random() * (max - min) + min)
+};
 
-//дергающийся таймер
+function getRandomColor(){
+  const index = Math.floor(Math.random()*COLORS.length);
+  return COLORS[index];
+};
+
+board.addEventListener('click', event => {
+  if(event.target.classList.contains('circle')){
+    score ++;
+    event.target.remove();
+    createRandomSpot();
+  }
+});
